286ebf69d97b61c2cf146282c4dd205d2a8ab642
Bisista Shrestha <sbisista@gmail.com>
2026-09-15 10:37:14 +0530

    2D Arrays

--- 9.2D Arrays/spiralMatrix.cpp
+++ 9.2D Arrays/spiralMatrix.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 int main(){
-    int arr[5][5]={
-        {1,2,3,4,5},
-        {6,7,8,9,10},
-        {11,12,13,14,15},
-        {16,17,18,19,20},
-        {21,22,23,24,25}
+    int arr[3][4]={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
     };
-    int n=5,m=5;
+    int n=3,m=4;
 
     int srow=0,scol=0,erow=n-1,ecol=m-1;
     while(srow<=erow && scol<=ecol){
